6b05fa92cdd6a90c8bc1938acef8a5f43a956a98
shambhaviakhouri <shambhavi.akhouri1012@gmail.com>
2024-10-11 23:10:03 +0530

    added timer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import About from "./components/About/About.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Timer from "./components/Timer/Timer.jsx";  
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Timer />  
       <About />
     </div>
   );

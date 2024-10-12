d489751734f22941b880e8a8e7fc6d8258ad21ce
adityasankar5 <adityasankarsengupta111@gmail.com>
2024-10-12 16:03:59 +0530

    outro section added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import About from "./components/About/About.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Timer from "./components/Timer/Timer.jsx";  
+import Timer from "./components/Timer/Timer.jsx";
+import Outro from "./components/Hero/Outro.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Timer />  
+      <Timer />
       <About />
+      <Outro />
     </div>
   );
 };

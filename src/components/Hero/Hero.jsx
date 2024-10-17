078412610ab6e3fd74f70a091aaec3a32e76155e
Aditya <adityag860libra@gmail.com>
2024-10-17 21:36:19 +0530

    Make iOS responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ const NeonButton = ({ text = 'Register Now' }) => {
 
   return (
     <a href="https://devfolio.co/" target="_blank" rel="noopener noreferrer">
-      <button onClick={playSound} className="px-6 py-3 bg-orange-500 bg-opacity-20 border-2 border-orange-500 rounded-md text-orange-500 text-xl font-semibold relative overflow-hidden group transition-all duration-300 hover:bg-opacity-30 hover:scale-105">
+      <button onClick={playSound} className="px-6 py-3 bg-orange-500 bg-opacity-20 border-2 border-orange-500 rounded-md text-orange-500 text-xl font-semibold relative overflow-hidden group transition-all duration-300 hover:bg-opacity-30 hover:scale-105 scale-125 md:scale-100">
         <span style={{ fontFamily: 'Hitmarker' }} className="relative z-10">{text}</span>
         <div className="absolute inset-0 bg-orange-500 opacity-30 blur-md group-hover:opacity-50 transition-opacity duration-300 animate-pulse"></div>
         <audio ref={audioRef} src={aud} />
@@ -100,28 +100,29 @@ export const Hero = () => {
   }, []);
 
   return (
-    <div ref={heroRef} className="parent h-screen flex flex-col crosshair-cursor">
-      <video id="video" autoPlay loop muted>
-        <source src={targetVid} type='video/webm' />
-      </video>
-      <div className="hero flex flex-col flex-1 gap-10 h-1/2">
-        <div className="hero-title flex flex-col w-full text-5xl">
-          <span className='flex ml-8 justify-start'>HACK&nbsp;OF</span>
-          <span className="flex mr-2 justify-end">DUTY</span>
-        </div>
-        <div className="hero-context flex flex-row w-full">
-          <div className="hero-context-left flex flex-col gap-2 items-center justify-center text-9xl md:text-[12rem] lg:text-[15rem]">
-            <span style={{ textShadow: '0 0 20px rgba(255, 255, 255, 0.5)' }}>24h</span>
-            <span style={{ textShadow: '0 0 20px rgba(255, 255, 255, 0.5)' }}>28-29</span>
-            <span style={{ textShadow: '0 0 20px rgba(255, 255, 255, 0.5)' }}>OCTOBER</span>
-          </div>
-          <div className="hero-context-right px-24">
-            <NeonButton />
-          </div>
-        </div>
-      </div>
-      <img src={Image} alt="Image" className='image absolute bottom-0 left-1/2 -translate-x-1/2 z-10 w-full md:w-3/4 lg:w-1/2' />
+<div ref={heroRef} className="parent h-screen flex flex-col crosshair-cursor">
+  <video id="video" autoPlay loop muted>
+    <source src={targetVid} type='video/webm' />
+  </video>
+  <div className="hero flex flex-col flex-1 gap-10 h-1/2">
+    <div className="hero-title flex flex-col w-full text-5xl">
+      <span className='flex ml-8 justify-start'>HACK&nbsp;OF</span>
+      <span className="flex mr-2 justify-end">DUTY</span>
     </div>
+    <div className="hero-context flex flex-col md:flex-row w-full">
+      <div className="hero-context-left flex flex-col gap-2 items-center justify-center text-9xl md:text-[12rem] lg:text-[15rem]">
+        <span style={{ textShadow: '0 0 20px rgba(255, 255, 255, 0.5)' }}>24h</span>
+        <span style={{ textShadow: '0 0 20px rgba(255, 255, 255, 0.5)' }}>28-29</span>
+        <span style={{ textShadow: '0 0 20px rgba(255, 255, 255, 0.5)' }}>OCTOBER</span>
+      </div>
+      <div className="hero-context-right flex justify-center md:px-24 md:justify-end mt-8 md:mt-0">
+        <NeonButton />
+      </div>
+    </div>
+  </div>
+  <img src={Image} alt="Image" className='image absolute bottom-0 left-1/2 -translate-x-1/2 z-10 w-full md:w-3/4 lg:w-1/2' />
+</div>
+
   );
 };
 

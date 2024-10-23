68a95c482cad5e7d80dc246d485b855bf5cae43d
Aditya <adityag860libra@gmail.com>
2024-10-23 15:51:20 +0530

    Add Aptos logo to sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -7,6 +7,7 @@ import ACM from '../../assets/ACM.png';
 import Devfolio from '../../assets/Devfolio.png';
 import Polygon from '../../assets/Polygon.png';
 import ETHIndia from '../../assets/ETHIndia.png';
+import Aptos from '../../assets/Aptos.png';
 import './Sponsors.css';
 
 const Sponsors = () => {
@@ -42,6 +43,9 @@ const Sponsors = () => {
             <div className="slide">
               <img className="sponsor-img" src={ETHIndia} alt="Img" />
             </div>
+            <div className="slide">
+              <img className="sponsor-img" src={Aptos} alt="Img" />
+            </div>
 
             <div className="slide">
               <img className="sponsor-img" src={SIGKDD} alt="Img" />
@@ -63,6 +67,9 @@ const Sponsors = () => {
             </div>
             <div className="slide">
               <img className="sponsor-img" src={ETHIndia} alt="Img" />
+            </div>
+            <div className="slide">
+              <img className="sponsor-img" src={Aptos} alt="Img" />
             </div>
           </div>
         </div>
